18b41d44499b0c5284394638b4ea707f24dcfc25
rivaalms <rvalms@icloud.com>
2023-10-29 01:18:21 +0700

    feat(sanctum): configure sanctum handler & fallback

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,24 +7,33 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+   /**
+    * The list of the inputs that are never flashed to the session on validation exceptions.
+    *
+    * @var array<int, string>
+    */
+   protected $dontFlash = [
+      'current_password',
+      'password',
+      'password_confirmation',
+   ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+   /**
+    * Register the exception handling callbacks for the application.
+    */
+   public function register(): void
+   {
+      $this->reportable(function (Throwable $e) {
+         //
+      });
+
+      $this->renderable(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+         if ($request->is('api/*')) {
+            return response()->json([
+               'success' => false,
+               'data' => 'Unauthenticated'
+            ], 401);
+         }
+      });
+   }
 }
