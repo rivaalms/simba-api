e7339293f994c3db59f5af2209a6813fec60d81d
rivaalms <rvalms@icloud.com>
2023-10-31 22:35:43 +0700

    feat: get schools

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\School;
 use Illuminate\Http\Request;
 
 class SchoolController extends Controller
 {
-    //
+   public function getSchool() {
+      $user = new User;
+      $schools = $user->where('userable_type', $user->userableType['school'])->get();
+      foreach($schools as $s) {
+         $s->userable;
+      }
+      return $this->apiResponse($schools);
+   }
 }
