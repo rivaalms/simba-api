0f78dffb0a18a1d327f264eeb51a2523751afd50
rivaalms <rvalms@icloud.com>
2023-10-31 22:35:07 +0700

    chore: create model factories

--- database/factories/DataFactory.php
+++ database/factories/DataFactory.php
@@ -9,15 +9,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class DataFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+   /**
+    * Define the model's default state.
+    *
+    * @return array<string, mixed>
+    */
+   public function definition(): array
+   {
+      return [
+         'data_type_id' => mt_rand(1, 34),
+         'school_id' => mt_rand(1, 2),
+         'path' => $this->faker->sentence,
+         'year' => '2022-2023',
+         'data_status_id' => mt_rand(1, 4)
+      ];
+   }
 }
--- database/factories/SchoolFactory.php
+++ database/factories/SchoolFactory.php
@@ -9,15 +9,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class SchoolFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+   /**
+    * Define the model's default state.
+    *
+    * @return array<string, mixed>
+    */
+   public function definition(): array
+   {
+      $school_type_id = mt_rand(1, 2);
+      $supervisor_id = 1;
+      return compact('school_type_id', 'supervisor_id');
+   }
 }
