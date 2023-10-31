3f77d464e9f0fd4b7ff1d22359a265f15a4e8a99
rivaalms <rvalms@icloud.com>
2023-10-31 23:15:12 +0700

    perf: optimize get schools query

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -9,11 +9,8 @@ use Illuminate\Http\Request;
 class SchoolController extends Controller
 {
    public function getSchool() {
-      $user = new User;
-      $schools = $user->where('userable_type', $user->userableType['school'])->get();
-      foreach($schools as $s) {
-         $s->userable;
-      }
+      $schools = User::where('userable_type', 'school')->get();
+      foreach ($schools as $s) $s->userable;
       return $this->apiResponse($schools);
    }
 }
