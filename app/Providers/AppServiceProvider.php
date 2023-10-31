eaf2fd2a42538f5fdf43676cc548dfe7be6ebfc5
rivaalms <rvalms@icloud.com>
2023-10-31 23:13:18 +0700

    perf: polymorphic aliasing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,29 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+   /**
+    * Register any application services.
+    */
+   public function register(): void
+   {
+      //
+   }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+   /**
+    * Bootstrap any application services.
+    */
+   public function boot(): void
+   {
+      Relation::enforceMorphMap([
+         'school' => "App\Models\School",
+         'supervisor' => "App\Models\Supervisor",
+         'officer' => "App\Models\Officer",
+         'user' => "App\Models\User"
+      ]);
+   }
 }
