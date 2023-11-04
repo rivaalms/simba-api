dca4c76fd8253fd4e501b10fe7577598561e62b6
rivaalms <rvalms@icloud.com>
2023-11-04 08:05:16 +0700

    feat(model): add default eager loading on Data

--- app/Models/Data.php
+++ app/Models/Data.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,7 +14,7 @@ class Data extends Model
 
    protected $with = [
       'school:id,supervisor_id',
-      'school.users:id,name,email,userable_type,userable_id',
+      'school.user:id,name,email,userable_type,userable_id',
       'status:id,name',
       'type:id,name,data_category_id',
       'type.category:id,name'
@@ -33,5 +34,21 @@ class Data extends Model
 
    public function redrafts() {
       return $this->hasMany(Redraft::class);
+   }
+
+   public function scopeFilter(Builder $query, Array $filters) {
+      $query->when($filters['school'] ?? false, fn (Builder $query, $school) => $query->where('school_id', $school));
+
+      $query->when($filters['category'] ?? false, fn (Builder $query, $category) =>
+         $query->whereHas('type', fn ($query) =>
+            $query->where('data_category_id', $category)
+         )
+      );
+
+      $query->when($filters['type'] ?? false, fn (Builder $query, $type) => $query->where('data_type_id', $type));
+
+      $query->when($filters['status'] ?? false, fn (Builder $query, $status) => $query->where('data_status_id', $status));
+
+      $query->when($filters['year'] ?? false, fn (Builder $query, $year) => $query->where('year', 'like', "%{$year}%"));
    }
 }
