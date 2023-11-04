923512d9157243e64b6fb81bbfb0bc7a2431d62a
rivaalms <rvalms@icloud.com>
2023-11-04 11:56:00 +0700

    feat(data): default sort data by latest

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Data;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class DataController extends Controller
 {
    public function getData(Request $request) {
-      $data = Data::filter(request(['school', 'type', 'category', 'status', 'year']))->paginate($request->per_page)->withQueryString();
+      $data = Data::filter(request(['school', 'type', 'category', 'status', 'year']))->latest()->paginate($request->per_page)->withQueryString();
 
       return $this->apiResponse($data);
    }
