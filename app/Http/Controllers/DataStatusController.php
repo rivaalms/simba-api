be84f0515ffa81a8f9238150bd8af9bd3872643d
rivaalms <rvalms@icloud.com>
2023-11-16 23:13:17 +0700

    feat: POST teachers & students

--- app/Http/Controllers/DataStatusController.php
+++ app/Http/Controllers/DataStatusController.php
@@ -7,6 +7,46 @@ use Illuminate\Http\Request;
 
 class DataStatusController extends Controller
 {
+   public function get() {
+      $status = DataStatus::all();
+      return $this->apiResponse($status);
+   }
+
+   public function create(Request $request) {
+      $validator = $request->validate([
+         'name' => 'required|unique:data_statuses,name'
+      ]);
+
+      $status = DataStatus::create([
+         'name' => $validator['name']
+      ]);
+
+      return $this->apiResponse($status, 'Data status created successfully', 201);
+   }
+
+   public function update(Request $request, int $id) {
+      $status = DataStatus::find($id);
+
+      $validator = $request->validate([
+         'name' => 'required'
+      ]);
+
+      $status->update([
+         'name' => $validator['name']
+      ]);
+
+      return $this->apiResponse(true, 'Data status updated successfully');
+   }
+
+   public function delete(Request $request) {
+      $validator = $request->validate([
+         'id' => 'required'
+      ]);
+
+      DataStatus::find($validator['id'])->delete();
+      return $this->apiResponse(true, 'Data status has been deleted');
+   }
+
    public function getDataStatusOptions () {
       $status = DataStatus::select('name as label', 'id as value')->get();
       return $this->apiResponse($status);
--- app/Http/Controllers/SchoolStudentController.php
+++ app/Http/Controllers/SchoolStudentController.php
@@ -12,4 +12,21 @@ class SchoolStudentController extends Controller
       $students = SchoolStudent::where('school_id', $request->school_id)->filter(request(['year']))->orderBy('updated_at', 'desc')->get();
       return $this->apiResponse($students);
    }
+
+   public function create(Request $request) {
+      $validator = $request->validate([
+         'school_id' => 'required',
+         'year' => 'required',
+         'grade' => 'required',
+         'religion_id' => 'required',
+         'count' => 'required|integer|numeric'
+      ]);
+
+      $students = SchoolStudent::where('school_id', $validator['school_id'])->where('year', 'like', '%'.$validator['year'].'%')->where('grade', $validator['grade'])->first();
+
+      if ($students) $students->update($validator);
+      else SchoolStudent::create($validator);
+
+      return $this->apiResponse(true, 'Data siswa berhasil ditambahkan');
+   }
 }
