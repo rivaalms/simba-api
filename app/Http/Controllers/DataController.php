0c64f8f59871d8d290f5d840635800a862c2aaf4
rivaalms <rvalms@icloud.com>
2024-02-09 19:42:46 +0700

    refactor: optimizing count data

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -89,50 +89,100 @@ class DataController extends Controller
       $user = request()->user();
       $isAdmin = !$user->userable_type;
 
-      $query = Data::filter(request(['year']))->yearRange(request(['start_year', 'end_year']))->get();
-      $status = DataStatus::all();
-      $category = DataCategory::all();
-      $type = DataType::all();
+      $allCategories = DataCategory::all();
+      $allTypes = DataType::without(['category'])->get();
+      $allStatuses = DataStatus::all();
+
+      $totalQuery = Data::filter(request(['year']))->yearRange(request(['start_year', 'end_year']));
 
       if (!$isAdmin) {
          switch ($user->userable_type) {
             case 'school':
-               $query = $query->where('school_id', $user->userable_id);
+               $total = $totalQuery->where('school_id', $user->userable_id)->count();
                break;
+            case 'supervisor':
+               $total = $totalQuery->whereHas('school', function ($query) use ($user) {
+                  $query->where('supervisor_id', $user->userable_id);
+               })->count();
             default:
+               $total = $totalQuery->count();
                break;
          }
       }
 
-      $total = $query->count();
+      $data = Data::without(['type', 'status', 'school'])->filter(request(['year']))->yearRange(request(['start_year', 'end_year']))
+         ->when(!$isAdmin, function ($query) use ($user) {
+            switch ($user->userable_type) {
+               case 'school':
+                  $query->where('school_id', $user->userable_id);
+                  break;
+               case 'supervisor':
+                  $query->whereHas('school', function ($query) use ($user) {
+                     $query->where('supervisor_id', $user->userable_id);
+                  });
+                  break;
+               default: break;
+            }
+         })
+         ->selectRaw('data.id, data_types.name as type, data_categories.name as category')
+         ->leftJoin('data_types', 'data.data_type_id', '=', 'data_types.id')
+         ->leftJoin('data_categories', 'data_types.data_category_id', '=', 'data_categories.id')
+         ->get();
 
-      $data_by_status = array();
-      foreach ($status as $s) {
-         array_push($data_by_status, array(
-            'name' => $s->name,
-            'count' => $query->where('data_status_id', $s->id)->count()
-         ));
-      }
+      foreach ($allCategories as $c) {
+         $cData = $data->where('category', $c->name);
+         $cCount = $cData->groupBy('type')->map(function ($type) {
+            return $type->count();
+         })->sum();
 
-      $data_by_category = array();
-      foreach ($category as $c) {
-         $cSum = 0;
-         $data_by_type = array();
-         $_type = $type->where('data_category_id', $c->id);
-         foreach ($_type as $t) {
-            $_total = $query->where('data_type_id', $t->id)->count();
-            $cSum += $_total;
-            array_push($data_by_type, array(
+         $tInC = $allTypes->where('data_category_id', $c->id);
+         $data_by_type = [];
+
+         foreach ($tInC as $t) {
+            $tData = $cData->where('type', $t->name);
+            $tCount = $tData->count();
+
+            $data_by_type[] = [
                'name' => $t->name,
-               'count' => $_total
-            ));
+               'count' => $tCount
+            ];
          }
-         array_push($data_by_category, array(
+
+         $data_by_category[] = [
             'name' => $c->name,
-            'total' => $cSum,
+            'count' => $cCount,
             'data_by_type' => $data_by_type
-         ));
+         ];
       }
+
+      $byStatus = Data::without(['type', 'status', 'school'])->filter(request(['year']))->yearRange(request(['start_year', 'end_year']))
+         ->when(!$isAdmin, function ($query) use ($user) {
+            switch ($user->userable_type) {
+               case 'school':
+                  $query->where('school_id', $user->userable_id);
+                  break;
+               case 'supervisor':
+                  $query->whereHas('school', function ($query) use ($user) {
+                     $query->where('supervisor_id', $user->userable_id);
+                  });
+                  break;
+               default: break;
+            }
+         })
+         ->selectRaw('count(data.id) as count, data_statuses.name as name')
+         ->leftJoin('data_statuses', 'data.data_status_id', '=', 'data_statuses.id')
+         ->groupBy('data_statuses.name')
+         ->get();
+
+         $data_by_status = array();
+         foreach ($allStatuses as $s) {
+            $sData = $byStatus->where('name', $s->name)->first();
+
+            $data_by_status[] = [
+               'name' => $s->name,
+               'count' => $sData->count
+            ];
+         }
 
       return $this->apiResponse(array_merge(compact('total', 'data_by_status', 'data_by_category'), request(['start_year', 'end_year'])));
    }
