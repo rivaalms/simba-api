c05b1be5bf8eb7710c9c0083b4d0c8e96f6bb2bf
rivaalms <rvalms@icloud.com>
2024-03-14 21:58:12 +0700

    build: map morph models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
     */
    public function boot(): void
    {
-      //
+      Relation::enforceMorphMap([
+         'school' => "App\Models\School",
+         'supervisor' => "App\Models\Supervisor",
+         'officer' => "App\Models\Officer",
+         'user' => "App\Models\User"
+      ]);
    }
 }
