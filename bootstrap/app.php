3bfd5218050ef3271bd27ecd210fb78636e4a796
rivaalms <rvalms@icloud.com>
2024-03-14 23:17:20 +0700

    feat: auth execption

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,10 +10,20 @@ return Application::configure(basePath: dirname(__DIR__))
       web: __DIR__ . '/../routes/web.php',
       commands: __DIR__ . '/../routes/console.php',
       health: '/up',
+      api: __DIR__ . '/../routes/api.php',
+      apiPrefix: 'api',
    )
    ->withMiddleware(function (Middleware $middleware) {
       $middleware->statefulApi();
    })
    ->withExceptions(function (Exceptions $exceptions) {
-      //
+      $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, Request $request) {
+         if ($request->is('api/*')) {
+            return response()->json([
+               'success' => false,
+               'message' => 'Unauthenticated',
+               'data' => null
+            ]);
+         }
+      });
    })->create();
